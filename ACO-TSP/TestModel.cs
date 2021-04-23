9e097abdbbe250e477192d1fe97e1e452eac9199
DESKTOP-Q0VFM82\phung <phunghathao1999@gmail.com>
2021-04-23 16:22:42 +0700

    first commit

---
+++ ACO-TSP/TestModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACO_TSP
+{
+    public class TestModel
+    {
+        public string namefile { get; set; }
+        //public List<List<int>> datas { get; set; }
+        public int[,] datas { get; set; }
+
+        public TestModel(string namefile, int[,] datas)
+        {
+            this.namefile = namefile;
+            this.datas = datas;
+        }
+    }
+}
