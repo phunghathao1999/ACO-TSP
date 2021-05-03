59b29cf2c33ba89d044e3b192e4fee5c848c8c9f
DESKTOP-Q0VFM82\phung <phunghathao1999@gmail.com>
2021-05-03 17:00:08 +0700

    complete

--- ACO-TSP/AcoTsp.cs
+++ ACO-TSP/AcoTsp.cs
@@ -10,21 +10,21 @@ namespace ACO_TSP
         {
             
             List<DistModel> distModels = new List<DistModel>();
-            distModels = readForder();
+            distModels = readFolder();
             foreach (DistModel distModel in distModels)
             {
                 int row = distModel.datas.GetLength(0);
                 int colums = distModel.datas.Length / row;
                 if (row == colums)
-                    for(int i = 0; i<30; i++)
+                    for (int i = 0; i < 30; i++)
                         CostMatrix(distModel, row);
 
             }
         }
 
-        public static List<DistModel> readForder()
+        public static List<DistModel> readFolder()
         {
-            List<DistModel> testModels = new List<DistModel>();
+            List<DistModel> distModels = new List<DistModel>();
             string folderPath = @"C:\Users\phung\Desktop\ACO-TSP\ACO-TSP\test\";
             foreach (string file in Directory.EnumerateFiles(folderPath, "*.txt"))
             {
@@ -63,18 +63,48 @@ namespace ACO_TSP
                     k++;
                 }
 
-                testModels.Add(new DistModel(filename, datas));
+                distModels.Add(new DistModel(filename, datas));
             }
-            return testModels;
+            return distModels;
         }
+
+        public static void writeFolder(string nameFile, int[] bestTour, int cost)
+        {
+            string folder = @"C:\Users\phung\Desktop\ACO-TSP\ACO-TSP\result\";
+            string fileName = nameFile.Split("_")[0] + "_s.txt";
+            string fullPath = folder + fileName;
+
+            
+            try
+            {
+                string[] tourString = new string[bestTour.Length + 1];
+                tourString[0] = cost.ToString();
+                for (int i = 1; i < bestTour.Length + 1; i++)
+                    tourString[i] = bestTour[i - 1].ToString();
+
+                string[] readLine = File.ReadAllLines(fullPath);
+                if (cost < Int32.Parse(readLine[0]))
+                    File.WriteAllLines(fullPath, tourString);
+            }
+            catch (FileNotFoundException e)
+            {
+                string[] tourString = new string[bestTour.Length + 1];
+                tourString[0] = cost.ToString();
+                for (int i = 1; i < bestTour.Length + 1; i++)
+                    tourString[i] = bestTour[i - 1].ToString();
+
+                File.WriteAllLines(fullPath, tourString);
+            }
+        }
+
         public static void CostMatrix(DistModel distModel, int noNodes)
         {
             int noAnts = 20;
             double alfa = 1.0;
             double beta = 3.0;
             double globalEvapRate = 0.5;
-            bool doOpt2 = false;
-            bool doOpt3 = false;
+            bool doOpt2 = true;
+            bool doOpt3 = true;
             MaxMinAS maxMinAS = new MaxMinAS(noNodes, noAnts, alfa, beta, globalEvapRate, doOpt2, doOpt3, 1000);
 
             maxMinAS.initData();
@@ -88,11 +118,7 @@ namespace ACO_TSP
             maxMinAS.updatePheromones();
             maxMinAS.setIteration(maxMinAS.getIteration() + 1);
 
-            int[] bestTour = maxMinAS.getBestSoFarTour();
-            for (int i = 0; i < bestTour.Length; i++)
-                Console.WriteLine("a: " + bestTour[i]);
-            Console.WriteLine("cost: " + maxMinAS.computeTourLength(bestTour));
-
+            writeFolder(distModel.namefile, maxMinAS.getBestSoFarTour(), maxMinAS.computeTourLength(maxMinAS.getBestSoFarTour()));
         }
 
         public static void OxygenCoordinates(DistModel distModel, int noNodes, int noAnts)
